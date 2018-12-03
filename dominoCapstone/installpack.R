ae148ca99011c3788a6dcde7568fa8fb105c8c90
roiseal <35386272+roiseal@users.noreply.github.com>
2018-12-02 21:07:49 -0500

    Add files via upload

---
+++ dominoCapstone/installpack.R
@@ -0,0 +1,24 @@
+################# ~~~~~~~~~~~~~~~~~ ######## ~~~~~~~~~~~~~~~~~ #################
+##                                                                            ##
+##                        Data Science Capstone Project                       ##
+##                                                                            ##            
+##                                   Alla Roisenberg                          ##
+##                                                                            ##
+##           Github Repo: https://github.com/roiseal/CapstoneCoursera      ##
+##                                                                            ##
+################# ~~~~~~~~~~~~~~~~~ ######## ~~~~~~~~~~~~~~~~~ #################
+
+install.packages(c("h2o",
+"RWekajars",
+"ggplot2",
+"qdapDictionaries",
+"qdapRegex",
+"qdapTools",
+"RColorBrewer",
+"qdap",
+"NLP",
+"tm",
+"SnowballC",
+"slam",
+"RWeka",
+"rJava"))
