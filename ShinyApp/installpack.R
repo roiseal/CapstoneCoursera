45b1b3b297d11a93d5f3deb3cad62c580754c2ae
roiseal <35386272+roiseal@users.noreply.github.com>
2018-12-02 20:40:29 -0500

    Add files via upload

---
+++ ShinyApp/installpack.R
@@ -0,0 +1,12 @@
+################# ~~~~~~~~~~~~~~~~~ ######## ~~~~~~~~~~~~~~~~~ #################
+##                                                                            ##
+##                        Data Science Capstone Project                       ##
+##                                                                            ##            
+##                                   Alla Roisenberg                          ##
+##                                                                            ##
+##           Github Repo: https://github.com/roiseal/CapstoneCoursera      ##
+##                                                                            ##
+################# ~~~~~~~~~~~~~~~~~ ######## ~~~~~~~~~~~~~~~~~ #################
+
+install.packages(c("shinythemes","shiny","stylo","tm","stringr","markdown"))
+
